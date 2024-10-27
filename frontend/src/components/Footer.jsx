9d98208b9e52f651d04abeee3e4c2125d2eba308
Solange Benítez Saavedra <154397099+Furufufa@users.noreply.github.com>
2024-10-27 17:12:47 -0300

    Add files via upload
    Agrego componente Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-light shadow-sm py-3">
+      <div className="container text-center">
+        <h3 className="mb-2">Somos proveedores especializados en industria veterinaria, atendemos a hospitales y a particulares</h3>
+        <a href="/contact" className="text-dark text-decoration-none">contactostore@rapivet.com</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
